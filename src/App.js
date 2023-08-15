392b940966978c7f067131b1763e86a5deca7b40
Nami96 <nabilnazmi96@gmail.com>
2023-08-16 01:16:01 +0800

    remove unnecessary stuff like logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import TodoList from './TodoList';
 
@@ -6,16 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <TodoList></TodoList>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
