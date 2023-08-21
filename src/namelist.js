935f91646898115468308a13248a16d12f575102
Nami96 <nabilnazmi96@gmail.com>
2023-08-22 00:01:19 +0800

    Add naming list function

---
+++ src/namelist.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+
+const GenList = () => {
+    const [nameLists, setNameLists] = useState([])
+    const [inputVal, setInputVal] = useState('')
+
+    const handleInputVal = (e) => {
+        setInputVal(e.target.value)
+    }
+
+    const handleAddNameList = () => {
+        if(inputVal.trim() !== '') {
+            setNameLists([...nameLists, { name:inputVal, item: TodoList}])
+            setInputVal('')
+        }
+    }
+
+    const handleDelList = (index) => {
+        const newNameLists = [...nameLists]
+        newNameLists.splice(index,1)
+        setNameLists(newNameLists)
+    }
+
+    return (
+        <div>
+            <div className="input-group mb-3">
+            <input className="form-control"
+                type="text"
+                value={inputVal}
+                onChange={handleInputVal}
+                >
+                </input>
+            <button className="btn btn-outline-secondary"
+                onClick={handleAddNameList}
+                >
+                Create List
+            </button>
+            </div>
+            <div className="list-card">
+                {nameLists.map( (nameList,index) => (
+                    <div key={index}>
+                        <h3 className="listName">{nameList.name} List</h3>
+                        <ul>
+                            <TodoList></TodoList>
+                        </ul>
+                        <button className="btn btn-danger custom-button" id='del-btn custom'
+                            onClick={handleDelList}
+                        >Delete List</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default GenList;
