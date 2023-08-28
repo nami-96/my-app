db002959ed9d546de112572e2c2c4a9a452c9aed
Nami96 <nabilnazmi96@gmail.com>
2023-08-28 20:07:32 +0800

    wip

--- src/namelist.js
+++ src/namelist.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import TodoList from "./TodoList";
 
 const GenList = () => {
-    const [nameLists, setNameLists] = useState([])
+    const [list, setList] = useState([{
+        name:'',
+        item:['']
+    }])
+
     const [inputVal, setInputVal] = useState('')
-    const [todos, setTodos] = useState([]);
 
     const handleInputVal = (e) => {
         setInputVal(e.target.value)
@@ -12,15 +15,15 @@ const GenList = () => {
 
     const handleAddNameList = () => {
         if(inputVal.trim() !== '') {
-            setNameLists([...nameLists, {name:inputVal}])
+            setList([...list, {name:inputVal}])
             setInputVal('')
         }
     }
 
     const handleDelList = (index) => {
-        const newNameLists = [...nameLists]
-        newNameLists.splice(index,1)
-        setNameLists(newNameLists)
+        const newLists = [...list]
+        newLists.splice(index,1)
+        setList(newLists)
     }
 
     return (
@@ -39,20 +42,17 @@ const GenList = () => {
             </button>
             </div>
             <div className="list-card">
-                {nameLists.map( (nameList,index) => (
+                {list.map( (list,index) => (
                     <div key={index}>
-                        <h3 className="listName">{nameList.name} List</h3>
+                        <h3 className="listName">{list.name} List</h3>
                         <ul>
-                            <TodoList
-                            todos={todos}
-                            setTodos={setTodos}    
-                            ></TodoList>
                         </ul>
                         <button className="btn btn-danger custom-button" id='del-btn custom'
                             onClick={handleDelList}
                         >Delete List</button>
                     </div>
                 ))}
+
             </div>
             <div className="card custom">
             <h2>Summary</h2>
--- src/summary.js
+++ src/summary.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import TodoList from './TodoList';
 
 const List = [
-    {id: 0,
+    {
      name:'',   
      listItem:['']},
     ]
